fa77f1f2b1d54ccc505465c4528a718359e0326f
sila1400 <sila1400@edu.mercantec.dk>
2023-11-22 13:55:02 +0100

    d

---
+++ Domain Models/Admin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models
+{
+    internal class Admin
+    {
+        public int AdminId;
+       
+        
+
+        
+        public void deleteAnyListing()
+        {
+            
+        }
+        public void deleteUser()
+        {
+           
+        }
+
+        public void editAnyListing()
+        {
+         
+        }
+
+        public void editUser()
+        {
+           
+        }
+
+        public void deleteReview()
+        {
+
+        }
+
+        public void editReview()
+        {
+
+        }
+    }
+}
