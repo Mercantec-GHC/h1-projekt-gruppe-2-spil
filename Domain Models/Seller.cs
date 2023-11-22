6fdfac4e1355ec363339ba33af3a7097c06b82ee
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-11-22 10:45:51 +0100

    Setup seller class but not done.

---
+++ Domain Models/Seller.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain_Models;
+
+public class Seller
+{
+    List<GameListing> listings { get; set; }
+    float rating { get; set; }
+    List<string> reviews { get; set; }
+    string addressLine { get; set; }
+
+    public void deleteListing(int listingId)
+    {
+
+    }
+
+    public void editListing(int listingId)
+    {
+
+    }
+
+    public void createLising(GameListing listing)
+    {
+
+    }
+
+}
