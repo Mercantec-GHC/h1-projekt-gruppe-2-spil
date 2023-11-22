c9801855b171133b017122c47a1d1a42fc0fb3cd
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-11-22 13:15:47 +0100

    Created the review class with a createview func

---
+++ Domain Models/Review.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain_Models;
+
+public class Review
+{
+    public int reviewId { get; set; }
+    public string reviewText { get; set; }
+    public int rating { get; set; }
+    public DateTime dateMade { get; set; }
+
+    public void createReview(int reviewId, string reviewText, int rating, DateTime dateMade, int userId, int sellerId)
+    {
+
+        reviewId = reviewId;
+        reviewText = reviewText;
+        rating = rating;
+        dateMade = dateMade;
+        userId = userId;
+        sellerId = sellerId;
+        
+
+    }
+}
