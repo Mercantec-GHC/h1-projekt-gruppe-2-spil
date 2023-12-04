a7e9256760fffa7c65edb9271a37d5a95ca6aa74
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-12-04 14:46:46 +0100

    just added a test user in sql script

--- SQL-Scripts/script.sql
+++ SQL-Scripts/script.sql
@@ -1,2 +1,7 @@
 --@block Her kan i skrive SQL scripts til at oprette jeres DB
-SELECT * FROM sql
+CREATE TABLE USERS (
+    ID INT NOT NULL IDENTITY(1,1),
+    USERNAME VARCHAR(255) NOT NULL,
+    PASSWORD VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
