c5710033d42dc76ffa795bb494e2fe53cf53cb95
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-12-07 11:01:34 +0100

    added connectionstring, fix dependency loop

--- BlazorApp/DataBase/DataBaseConnection.cs
+++ BlazorApp/DataBase/DataBaseConnection.cs
@@ -9,25 +9,26 @@ public static void DataBaseConnect(){
     string? dbUser = System.Environment.GetEnvironmentVariable("ASPNETCORE_DBUsername");
     string? dbPassword = System.Environment.GetEnvironmentVariable("ASPNETCORE_DBPassword");
     string? dbCatalog = System.Environment.GetEnvironmentVariable("ASPNETCORE_DBName");
+    string? dbConnectionString = System.Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTIONSTRING");
 try 
             { 
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
-                builder.DataSource = dbSource; 
+                /*builder.DataSource = dbSource; 
                 builder.UserID = dbUser;            
                 builder.Password = dbPassword;     
-                builder.InitialCatalog = dbCatalog;
+                builder.InitialCatalog = dbCatalog;*/
 
                 //builder.ConnectionString="Server=tcp:h1-grp2-dbserver.database.windows.net,1433;Initial Catalog=H1-Grp2-Blazor-Eksamen-Database;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication='Active Directory Default';";
          
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString = dbConnectionString))
                 {
                     Console.WriteLine("\nQuery data example:");
                     Console.WriteLine("=========================================\n");
                     
                     connection.Open();       
 
-                    /*String sql = "SELECT name, collation_name FROM sys.databases";
+                    String sql = "SELECT name, collation_name FROM sys.databases";
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
@@ -38,7 +39,7 @@ try
                                 Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
                             }
                         }
-                    }  */                  
+                    }                   
                 }
             }
             catch (SqlException e)
--- Domain Models/Seller.cs
+++ Domain Models/Seller.cs
@@ -1,6 +1,4 @@
 ﻿using System.Data.Common;
-using Microsoft.Data.SqlClient;
-using BlazorApp.DataBase;
 namespace Domain_Models;
 
 public class Seller : User
@@ -36,19 +34,7 @@ public class Seller : User
     }
 
     public void createLising(GameListing listing)
-    {
-        DataBaseConnection.DataBaseConnect();
-        String sql = "SELECT name, collation_name FROM sys.databases";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
-                    }    
+    {    
         if(listing != null)
         {
             listings.Add(listing);
